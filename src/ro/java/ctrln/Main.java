623b0e28dd66e999b423323d84086983392288f2
noeministor <noemiszabo31@gmail.com>
2021-03-08 08:58:25 +0200

    Adaugare multi-line-comments branch

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -2,7 +2,10 @@ package ro.java.ctrln;
 
 public class Main {
 //    Aceasta este metoda main care porneste o aplicatie Java
-    public static void main(String[] args) {
+    public static void main( /*arg este paramentrul metodei main  */String[] args) {
+        /* rand 1
+        rand 2
+        rand 3 */
         // Aici vrem sa afisam pe consola IntelliJ un mesaj ales de noi
 	    System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
