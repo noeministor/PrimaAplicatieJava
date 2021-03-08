f70860c5799b68b6c3e7677b804b9b0492a02be7
noeministor <noemiszabo31@gmail.com>
2021-03-08 08:41:26 +0200

    Adaugare singe-line-comments branch

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -1,8 +1,9 @@
 package ro.java.ctrln;
 
 public class Main {
-
+//    Aceasta este metoda main care porneste o aplicatie Java
     public static void main(String[] args) {
+        // Aici vrem sa afisam pe consola IntelliJ un mesaj ales de noi
 	    System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
 }
